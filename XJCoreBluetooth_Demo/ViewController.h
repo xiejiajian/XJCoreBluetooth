2eae5f2b8c7b00bb4d9c996d8bf9fe50b4c4e8ff
xiejiajian <xjj601215@163.com>
2016-07-21 11:34:11 +0800

    Initial Commit

---
+++ XJCoreBluetooth_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJCoreBluetooth_Demo
+//
+//  Created by sl on 16/7/21.
+//  Copyright © 2016年 xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XJCoreBluetooth_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XJCoreBluetooth_Demo
+//
+//  Created by sl on 16/7/21.
+//  Copyright © 2016年 xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
