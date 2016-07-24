a8c6ef6c76befb4a5b5d0bb0b5087a22a23d2b8f
xiejiajian <xjj601215@163.com>
2016-07-24 22:51:06 +0800

    bug fixes

---
+++ XJCoreBluetooth_Demo/Controller/XJDemoController.h
@@ -0,0 +1,13 @@
+//
+//  XJDemoController.h
+//  XJCoreBluetooth_Demo
+//
+//  Created by sl on 16/7/24.
+//  Copyright © 2016年 xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XJDemoController : UIViewController
+
+@end
---
+++ XJCoreBluetooth_Demo/Controller/XJDemoController.m
@@ -0,0 +1,37 @@
+//
+//  XJDemoController.m
+//  XJCoreBluetooth_Demo
+//
+//  Created by sl on 16/7/24.
+//  Copyright © 2016年 xie. All rights reserved.
+//
+
+#import "XJDemoController.h"
+
+@interface XJDemoController ()
+
+@end
+
+@implementation XJDemoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
